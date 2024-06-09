5583f0da9202b110ebf94e0ac550805f8b33193c
SeigoTakahashi <seigo2004takahashi@gmail.com>
2024-06-10 08:37:43 +0900

    ArrayTree final commit

--- AlgoImpl/src/array/ArrayTree.java
+++ AlgoImpl/src/array/ArrayTree.java
@@ -1,5 +1,9 @@
 package array;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ArrayTree {
 
 	int[] treeArray;
@@ -92,7 +96,6 @@ public class ArrayTree {
 		}
 		int left = 2 * target + 1;
 		int right = 2 * target + 2;
-		int parent = (target - 1) / 2;
 
 		if (treeArray[left] == -1 && treeArray[right] == -1) {
 			treeArray[target] = -1;
@@ -100,16 +103,118 @@ public class ArrayTree {
 		}
 
 		if (treeArray[left] != -1 && treeArray[right] == -1) {
-			ArrayQueue que = new ArrayQueue();
-			
+			int[] newTree = Arrays.copyOf(this.treeArray, this.treeArray.length);
+			int[] oldTree = Arrays.copyOf(this.treeArray, this.treeArray.length);
+			Queue<int[]> queue = new LinkedList<>();
+			int new_i = target;
+			int old_i = left;
+			queue.add(new int[] { old_i, new_i });
+
+			while (!queue.isEmpty()) {
+				int[] i = queue.poll();
+				old_i = i[0];
+				new_i = i[1];
+
+				newTree[new_i] = oldTree[old_i];
+				newTree[old_i] = -1;
+
+				int old_left = 2 * old_i + 1;
+				int old_right = 2 * old_i + 2;
+
+				int new_left = 2 * new_i + 1;
+				int new_right = 2 * new_i + 2;
+
+				if (oldTree[old_left] != -1) {
+					queue.add(new int[] { old_left, new_left });
+				}
+
+				if (oldTree[old_right] != -1) {
+					queue.add(new int[] { old_right, new_right });
+				}
+			}
+			this.treeArray = newTree;
+			return;
+
 		}
 
 		if (treeArray[left] == -1 && treeArray[right] != -1) {
+			int[] newTree = Arrays.copyOf(this.treeArray, this.treeArray.length);
+			int[] oldTree = Arrays.copyOf(this.treeArray, this.treeArray.length);
+			Queue<int[]> queue = new LinkedList<>();
+			int new_i = target;
+			int old_i = right;
+			queue.add(new int[] { old_i, new_i });
 
+			while (!queue.isEmpty()) {
+				int[] i = queue.poll();
+				old_i = i[0];
+				new_i = i[1];
+
+				newTree[new_i] = oldTree[old_i];
+				newTree[old_i] = -1;
+
+				int old_left = 2 * old_i + 1;
+				int old_right = 2 * old_i + 2;
+
+				int new_left = 2 * new_i + 1;
+				int new_right = 2 * new_i + 2;
+
+				if (oldTree[old_left] != -1) {
+					queue.add(new int[] { old_left, new_left });
+				}
+
+				if (oldTree[old_right] != -1) {
+					queue.add(new int[] { old_right, new_right });
+				}
+			}
+			this.treeArray = newTree;
+			return;
 		}
-		
-		if (treeArray[left] != -1 && treeArray[right] != -1) {
 
+		if (treeArray[left] != -1 && treeArray[right] != -1) {
+			int min_value = this.min(right);
+			int min_i = this.search(min_value);
+			int min_right = 2 * min_i + 2;
+			if(this.treeArray[min_right] == -1) {
+				this.treeArray[min_i] = -1;
+				this.treeArray[target] = min_value;
+				return;
+			}
+			
+			if(this.treeArray[min_right] != 1) {
+				int[] newTree = Arrays.copyOf(this.treeArray, this.treeArray.length);
+				int[] oldTree = Arrays.copyOf(this.treeArray, this.treeArray.length);
+				Queue<int[]> queue = new LinkedList<>();
+				int new_i = min_i;
+				int old_i = min_right;
+				queue.add(new int[] { old_i, new_i });
+
+				while (!queue.isEmpty()) {
+					int[] i = queue.poll();
+					old_i = i[0];
+					new_i = i[1];
+
+					newTree[new_i] = oldTree[old_i];
+					newTree[old_i] = -1;
+
+					int old_left = 2 * old_i + 1;
+					int old_right = 2 * old_i + 2;
+
+					int new_left = 2 * new_i + 1;
+					int new_right = 2 * new_i + 2;
+
+					if (oldTree[old_left] != -1) {
+						queue.add(new int[] { old_left, new_left });
+					}
+
+					if (oldTree[old_right] != -1) {
+						queue.add(new int[] { old_right, new_right });
+					}
+				}
+				this.treeArray = newTree;
+				this.treeArray[target] = min_value;
+				return;
+			}
 		}
 
 	}
